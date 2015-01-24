b5cee14d66dad31d869f28fc573889f460750c7a
mayk93 <mihai.mandrescu@gmail.com>
2015-01-24 09:26:38 +0200

    Work in Progress - Spawner
    Enemy deletion and explosion.

--- GooScript.cs
+++ GooScript.cs
@@ -6,23 +6,31 @@ public class GooScript : MonoBehaviour {
 	public GameObject sperm;
 	public GameObject empty;
 	public Sprite [] gooArray;
+	public ParticleSystem explosionEffect;
 
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector2 (sperm.rigidbody2D.position.x + Random.Range (-1f, 1f), sperm.rigidbody2D.position.y + 10);
-		InvokeRepeating ("spawnGoo", 0.3f , 0.3f);
+		transform.position = new Vector2 (sperm.rigidbody2D.position.x, sperm.rigidbody2D.position.y + 15);
+		InvokeRepeating ("spawnGoo", 2f , 2f);
 	}
 
 	void spawnGoo()
 	{
 		//gooArray [Random.Range (0, gooArray.Length - 1)]
-		GameObject clone = (GameObject)Instantiate (empty, new Vector3(transform.position.x,transform.position.y,transform.position.z-1), Quaternion.identity);
+		GameObject clone = (GameObject)Instantiate (empty, new Vector3(transform.position.x + Random.Range (-10f, 10f),transform.position.y + Random.Range (0f, 1f),transform.position.z-1), Quaternion.identity);
 		SpriteRenderer cloneSpriteRenderer = clone.AddComponent<SpriteRenderer>();
 		cloneSpriteRenderer.sprite = gooArray [Random.Range (0, gooArray.Length - 1)];
+		Rigidbody2D cloneRidgidBody2D = clone.AddComponent<Rigidbody2D> ();
+		cloneRidgidBody2D.gravityScale = 0.3f;
+		PolygonCollider2D clonePolygonCollider2D = clone.AddComponent<PolygonCollider2D> ();
+		SelfDestroy cloneSelfDestroy = clone.AddComponent<SelfDestroy> ();
+		cloneSelfDestroy.explosion = explosionEffect;
+
+		clone.transform.localScale = new Vector3 (Random.Range (3f, 7f), Random.Range (3f, 7f), Random.Range (0.5f, 1.5f));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector2 (sperm.rigidbody2D.position.x + Random.Range (-1f, 1f), sperm.rigidbody2D.position.y + 10);
+		transform.position = new Vector2 (sperm.rigidbody2D.position.x, sperm.rigidbody2D.position.y + 10);
 	}
 }
---
+++ SelfDestroy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestroy : MonoBehaviour {
+
+	public ParticleSystem explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnDestroy() 
+	{
+		ParticleSystem p = (ParticleSystem)Instantiate(explosion, transform.position, transform.rotation);
+		Destroy (p , 0.5f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Destroy (gameObject, 3);
+	}
+
+	void OnCollisionEnter2D()
+	{
+		Destroy (gameObject);
+	}
+}
