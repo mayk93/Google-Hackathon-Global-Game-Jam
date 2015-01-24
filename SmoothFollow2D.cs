34f5779ffc35120ffe8dd9a23a5c290198420837
mayk93 <mihai.mandrescu@gmail.com>
2015-01-24 04:58:01 +0200

    Work in Progress - Sperm Rotation Script
    Added basic rotation.

---
+++ SmoothFollow2D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public float targetY = 0f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 currentPosition = transform.position;
+		currentPosition.y = Mathf.Lerp (transform.position.y, targetY, 1 * Time.deltaTime);
+		transform.position = currentPosition;
+	}
+}
--- SpermMovement.cs
+++ SpermMovement.cs
@@ -5,27 +5,39 @@ public class SpermMovement : MonoBehaviour {
 
 	public GameObject spermObject;
 	public float velocityRatio = 1f;
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+	public GameObject emptyGameObject;
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		float lSpeed = 100.0f; // Set this to a value you like
-
 		if (Input.GetMouseButtonDown (0)) 
 		{
 			Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 mousePos2D = new Vector2( mouseWorldPos3D.x , mouseWorldPos3D.y );
 
-			spermObject.transform.rotation = Quaternion.Lerp ( spermObject.transform.rotation, Quaternion.Euler(mouseWorldPos3D), Time.deltaTime*lSpeed);
-
 			spermObject.rigidbody2D.velocity = (mousePos2D - spermObject.rigidbody2D.position) * velocityRatio;
 
-			//spermObject.rigidbody2D.transform.LookAt(mouseWorldPos3D);
+			float cameraDif = Camera.main.transform.position.y - transform.position.y;
+			float mouseX = Input.mousePosition.x;
+			float mouseY = Input.mousePosition.y;
+			Vector3 mWorldPos = new Vector3(mouseX, mouseY, cameraDif);
+			Vector3 mainPos = Camera.main.transform.position;
+			float diffX = mWorldPos.x - mainPos.x;
+			float diffY = mWorldPos.y - mainPos.y;
+			float angle = Mathf.Atan2(diffY, diffX) * Mathf.Rad2Deg;
+
+			Debug.Log(angle);
+
+			spermObject.rigidbody2D.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 50));
+
+			Vector3 cameraPosition = Camera.main.transform.position;
+
+			if(cameraPosition.y < mouseWorldPos3D.y)
+			{
+				Camera.main.GetComponent<SmoothFollow2D>().targetY = mouseWorldPos3D.y;
+			}
 		}
 	}
+
 }
