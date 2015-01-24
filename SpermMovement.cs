2588c4d0725420377a4044903f5dc3baafdf91d5
mayk93 <mihai.mandrescu@gmail.com>
2015-01-24 03:18:22 +0200

    Work in Progress - Sperm Movement Script
    Major rewrite coming.

--- SpermMovement.cs
+++ SpermMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class SpermMovement : MonoBehaviour {
 
 	public GameObject spermObject;
-	public float velocityRatio = 10f;
+	public float velocityRatio = 1f;
 	
 	// Use this for initialization
 	void Start () {
@@ -14,12 +14,18 @@ public class SpermMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		float lSpeed = 100.0f; // Set this to a value you like
+
 		if (Input.GetMouseButtonDown (0)) 
 		{
 			Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 mousePos2D = new Vector2( mouseWorldPos3D.x , mouseWorldPos3D.y );
-			
+
+			spermObject.transform.rotation = Quaternion.Lerp ( spermObject.transform.rotation, Quaternion.Euler(mouseWorldPos3D), Time.deltaTime*lSpeed);
+
 			spermObject.rigidbody2D.velocity = (mousePos2D - spermObject.rigidbody2D.position) * velocityRatio;
+
+			//spermObject.rigidbody2D.transform.LookAt(mouseWorldPos3D);
 		}
 	}
 }
