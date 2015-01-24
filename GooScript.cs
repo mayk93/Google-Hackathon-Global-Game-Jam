a010f74465cf3cf7fb87ff511f8e729ac9ffa0e6
mayk93 <mihai.mandrescu@gmail.com>
2015-01-24 18:02:33 +0200

    Work in Progress - Pulse
    Pulse for enemies.

--- GooScript.cs
+++ GooScript.cs
@@ -8,6 +8,7 @@ public class GooScript : MonoBehaviour {
 	public Sprite [] gooArray;
 	public ParticleSystem explosionEffect;
 
+
 	// Use this for initialization
 	void Start () {
 		transform.position = new Vector2 (sperm.rigidbody2D.position.x, sperm.rigidbody2D.position.y + 15);
@@ -19,8 +20,8 @@ public class GooScript : MonoBehaviour {
 		//gooArray [Random.Range (0, gooArray.Length - 1)]
 		GameObject clone = (GameObject)Instantiate (empty, new Vector3(transform.position.x + Random.Range (-5f, 5),transform.position.y + Random.Range (0f, 1f),transform.position.z-1), Quaternion.identity);
 		SpriteRenderer cloneSpriteRenderer = clone.AddComponent<SpriteRenderer>();
-
-		cloneSpriteRenderer.sprite = gooArray [Random.Range (0, gooArray.Length - 1)];
+		
+		//cloneSpriteRenderer.sprite = gooArray [Random.Range (0, gooArray.Length - 1)];
 
 		Rigidbody2D cloneRidgidBody2D = clone.AddComponent<Rigidbody2D> ();
 		cloneRidgidBody2D.gravityScale = 0.3f;
@@ -29,6 +30,8 @@ public class GooScript : MonoBehaviour {
 		cloneSelfDestroy.explosion = explosionEffect;
 
 		clone.transform.localScale = new Vector3 (Random.Range (1f, 3f), Random.Range (1f, 3f), Random.Range (0.5f, 1.5f));
+
+		Pulse pulseScript = clone.AddComponent<Pulse> ();
 	}
 	
 	// Update is called once per frame
---
+++ Pulse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour {
+
+	public Sprite [] gooArrayX;
+	public SpriteRenderer sprRend;
+	// Use this for initialization
+	void Start () {
+		gooArrayX = FindObjectOfType<GooScript>().gooArray;
+		sprRend = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		print (1);
+		sprRend.sprite = gooArrayX [1];
+
+		//Random.Range (0, gooArrayX.Length - 1)
+	}
+}
