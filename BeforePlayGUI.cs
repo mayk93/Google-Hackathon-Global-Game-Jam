45c1fce9490fa82b331d454cd30d69e0269b2069
mayk93 <mihai.mandrescu@gmail.com>
2015-01-25 15:59:01 +0200

    Finished Hackathon Coding
    Final Finish - Hopefully!

--- BeforePlayGUI.cs
+++ BeforePlayGUI.cs
@@ -10,6 +10,8 @@ public class BeforePlayGUI : MonoBehaviour {
 
 	public GameObject buttonStart;
 
+	public AudioClip audioClp;
+
 	// Use this for initialization
 	void Start () {
 		mySpr = textSprite.GetComponent<SpriteRenderer> ();
@@ -17,13 +19,16 @@ public class BeforePlayGUI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	public void Next ()
 	{
 		mySpr.sprite = nextSprite;
 		buttonStart.SetActive (true);
+
+		AudioSource ausrc = gameObject.GetComponent<AudioSource>();
+		audio.PlayOneShot (audioClp);
 	}
 
 	public void StartButtonFunction()
