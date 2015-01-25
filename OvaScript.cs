dae6aa3ddce9a98052dd50dea0491a181a2ba849
mayk93 <mihai.mandrescu@gmail.com>
2015-01-25 17:41:22 +0200

    Functioning 1
    Final

---
+++ OvaScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OvaScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		Collider2D colliderX = coll.collider;
+		if (colliderX.tag == "Player" ) 
+		{
+			Application.LoadLevel("Finished");
+		}
+	}
+}
---
+++ SpawnOva.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOva : MonoBehaviour {
+
+	public GameObject sperm;
+	public Sprite ova;
+
+	public GameObject empty;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector2 (sperm.rigidbody2D.position.x, sperm.rigidbody2D.position.y + 20);
+		InvokeRepeating ("SpawnOvaFunction", 5f , 5f);
+		//CallSpawnOvaFunction ();
+	}
+	/*	
+	void CallSpawnOvaFunction()
+	{
+		print ("01");
+		SpawnOvaFunction ();
+		print ("02");
+	}
+	*/
+	void SpawnOvaFunction()
+	{
+		//print (1);
+		//yield return new WaitForSeconds (5f);
+		//print (2);
+		GameObject ovaX = (GameObject)Instantiate (empty, new Vector3(transform.position.x ,transform.position.y + 5f,transform.position.z-1), Quaternion.identity);
+		SpriteRenderer ovaXR = ovaX.AddComponent<SpriteRenderer>();
+		ovaXR.sprite = ova;
+		Rigidbody2D ovaRidgidBody2D = ovaX.AddComponent<Rigidbody2D> ();
+		ovaRidgidBody2D.gravityScale = 0.1f;
+		PolygonCollider2D ovaPolygonCollider2D = ovaX.AddComponent<PolygonCollider2D> ();
+		OvaScript ovaXScript = ovaX.AddComponent<OvaScript> ();
+	}
+
+	void Update () {
+		transform.position = new Vector2 (sperm.rigidbody2D.position.x, sperm.rigidbody2D.position.y + 10);
+	}
+}
